ff0b1cd7776ee88ba3c23eb4bbdb90f6407c3611
aoaodev <aoaodev@outlook.com>
2026-04-29 15:49:42 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, AoAo! Another lovely day to learn!");
+decimal a = 42.1m; //explicit type 
+decimal b = 38.2m;
+decimal c = a + b;
+Console.WriteLine($"The answer is {c}");
